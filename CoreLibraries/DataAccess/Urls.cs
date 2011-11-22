5970aec83c0ecefb719f8fce98735247153cf4c7
Sean Dawson <sean.dawson@noxharmonium.com>
2011-11-22 23:39:57 +0000

    -Added tramtracker namespace to urls

--- CoreLibraries/DataAccess/Urls.cs
+++ CoreLibraries/DataAccess/Urls.cs
@@ -7,8 +7,9 @@ namespace RmitJourneyPlanner.CoreLibraries.DataAccess
 {
     public static class Urls
     {
-        public const string GeocodingApiUrl = @"http://maps.googleapis.com/maps/api/geocode/xml";
-        public const string DistanceApiUrl = @"http://maps.googleapis.com/maps/api/distancematrix/xml";
-        public const string TramTrackerUrl = @"http://ws.tramtracker.com.au/pidsservice/pids.asmx";
+        public const string GeocodingApiUrl         = @"http://maps.googleapis.com/maps/api/geocode/xml";
+        public const string DistanceApiUrl          = @"http://maps.googleapis.com/maps/api/distancematrix/xml";
+        public const string TramTrackerUrl          = @"http://ws.tramtracker.com.au/pidsservice/pids.asmx";
+        public const string TramTrackerNameSpace    = @"http://www.yarratrams.com.au/pidsservice/";    
     }
 }
