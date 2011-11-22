2c6a88e8de36fa01f682d1af92fa616338c39da7
Sean Dawson <sean.dawson@noxharmonium.com>
2011-11-22 22:23:46 +0000

    -Changed Stop to Tramstop to work on #1

--- CoreLibraries/Positioning/TramStop.cs
+++ CoreLibraries/Positioning/TramStop.cs
@@ -6,18 +6,19 @@ using System.Text;
 namespace RmitJourneyPlanner.CoreLibraries.Positioning
 {
     /// <summary>
-    /// Represents a  train station, a tram stop or a bus stop
-    /// as defined by the TransNET database. 
+    /// Represents a tram stop in the Yarra Trams database.
     /// </summary>
-    public class Stop : Location
+    public class TramStop : Location
     {
-        private int stopID;        
-        
+        private int stopID;
+        private int routeID;
+        private int routeDirection;
+
         /// <summary>
         /// Creates a new Stop object from the stop id in the TransNET database.
         /// </summary>
         /// <param name="stopID"></param>
-        public Stop(int stopID) : base(0,0)
+        public TramStop(int stopID) : base(0,0)
         {
             throw new NotImplementedException("TODO: Lookup stop id for location");
         }
