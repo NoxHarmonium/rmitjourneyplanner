d956ccb9e07426863a7eb0d9b28ed4704273860f
Sean Dawson <sean.dawson@noxharmonium.com>
2011-11-23 00:33:17 +0000

    -Reverted as it was stupid. The XMLDocument would grow to large over the course of the program

--- CoreLibraries/DataAccess/SOAP.cs
+++ CoreLibraries/DataAccess/SOAP.cs
@@ -26,13 +26,7 @@ namespace RmitJourneyPlanner.CoreLibraries.DataAccess
                                             "   </soap:Body>" +
                                             "</soap:Envelope>";
 
-        private static XmlDocument doc;
-
-        public SOAP()
-        {
-            doc = new XmlDocument();
-        }
-        
+       
         public static XmlDocument GetSoapTemplate()
         {
             XmlDocument doc = new XmlDocument();
@@ -40,15 +34,6 @@ namespace RmitJourneyPlanner.CoreLibraries.DataAccess
             return doc;
         }
 
-        public static XmlNode NewNode(string name)
-        {
-            return doc.CreateNode(XmlNodeType.Element, name,"");
-        }
-
-        public static XmlNode NewNode(string name, string xmlNamespace)
-        {
-            return doc.CreateNode(XmlNodeType.Element, name, xmlNamespace);
-        }
-
+       
     }
 }
