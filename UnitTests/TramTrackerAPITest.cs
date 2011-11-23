a51e68bb754e57d368eeae3c28d5bc0538723616
Sean Dawson <sean.dawson@noxharmonium.com>
2011-11-23 00:14:42 +0000

    -TramTrackerAPI now gets UUID from the webservice -Added unit test to check is UUID is correct.

---
+++ UnitTests/TramTrackerAPITest.cs
@@ -0,0 +1,81 @@
+﻿using RmitJourneyPlanner.CoreLibraries.DataAccess;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text.RegularExpressions;
+
+namespace UnitTests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for TramTrackerAPITest and is intended
+    ///to contain all TramTrackerAPITest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class TramTrackerAPITest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for Guid
+        ///</summary>
+        [TestMethod()]
+        public void GuidTest()
+        {
+            TramTrackerAPI target = new TramTrackerAPI(); 
+            string actual;
+            actual = target.Guid;
+            string expression = "[a-zA-Z0-9]{8}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{12}";
+            Assert.IsTrue(new Regex(expression).IsMatch(actual));
+        }
+    }
+}
