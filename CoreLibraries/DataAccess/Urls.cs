8b24ef38d7e64913897704bec40d18b6e246373f
Sean Dawson <sean.dawson@noxharmonium.com>
2011-11-23 05:10:07 +0000

    -Added more URLS

--- CoreLibraries/DataAccess/Urls.cs
+++ CoreLibraries/DataAccess/Urls.cs
@@ -8,17 +8,19 @@ namespace RmitJourneyPlanner.CoreLibraries.DataAccess
     public static class Urls
     {
         //Google Maps Strings
-        public const string GeocodingApiUrl         = @"http://maps.googleapis.com/maps/api/geocode/xml";
-        public const string DistanceApiUrl          = @"http://maps.googleapis.com/maps/api/distancematrix/xml";
+        public const string GeocodingApiUrl             = @"http://maps.googleapis.com/maps/api/geocode/xml";
+        public const string DistanceApiUrl              = @"http://maps.googleapis.com/maps/api/distancematrix/xml";
         
        
         
         //Yarra Trams Strings
         //  Global
-        public const string TramTrackerUrl          = @"http://ws.tramtracker.com.au/pidsservice/pids.asmx";
-        public const string TramTrackerNameSpace    = @"http://www.yarratrams.com.au/pidsservice/";
+        public const string TramTrackerUrl              = @"http://ws.tramtracker.com.au/pidsservice/pids.asmx";
+        public const string TramTrackerNameSpace        = @"http://www.yarratrams.com.au/pidsservice/";
         
         //  Actions
-        public const string GetNewClientGuid        = @"http://www.yarratrams.com.au/pidsservice/GetNewClientGuid";
+        public const string GetNewClientGuid            = @"http://www.yarratrams.com.au/pidsservice/GetNewClientGuid";
+        public const string GetDestinationsForAllRoutes = @"http://www.yarratrams.com.au/pidsservice/GetDestinationsForAllRoutes";
+        public const string GetDestinationsForRoute     = @"http://www.yarratrams.com.au/pidsservice/GetDestinationsForRoute";
     }
 }
