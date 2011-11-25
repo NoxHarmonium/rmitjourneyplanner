6e530b0cd0ee5ad9156f8cdf9cdcdcee88717292
Sean Dawson <sean.dawson@noxharmonium.com>
2011-11-25 04:01:52 +0000

    -Started adding a location cache -Worked on #5

---
+++ CoreLibraries/Caching/LocationCache.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="LocationCache.cs" company="RMIT University">
+//  By Sean Dawson
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace RmitJourneyPlanner.CoreLibraries.Caching
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using RmitJourneyPlanner.CoreLibraries.Positioning;
+
+    /// <summary>
+    /// Allows high speed access and searching of multiple locations.
+    /// </summary>
+    public class LocationCache
+    {
+       
+        public void AddCacheEntry(string id, Location location)
+        {
+
+        }
+
+    }
+}
