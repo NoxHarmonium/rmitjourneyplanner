66dc91c197435085d1abb46abfe4c1b03d2e6a3c
Sean Dawson <sean.dawson@noxharmonium.com>
2011-11-27 12:31:09 +0000

    -Added route cache -Need route datatype?

---
+++ CoreLibraries/Caching/RouteCache.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RmitJourneyPlanner.CoreLibraries.Caching
+{
+    /// <summary>
+    /// Caches route data for public transport.
+    /// </summary>
+    class RouteCache
+    {
+
+
+
+
+
+    }
+}
