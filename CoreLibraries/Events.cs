f22327d06b05b8eb1f982302659411e1be75d1fd
Sean Dawson <sean.dawson@noxharmonium.com>
2011-11-29 06:06:28 +0000

    -Added an events class to handle all the events in the project

---
+++ CoreLibraries/Events.cs
@@ -0,0 +1,33 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Events.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace RmitJourneyPlanner.CoreLibraries
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using DataProviders;
+
+    /// <summary>
+    /// Represents the data produced by a next iteration event.
+    /// </summary>
+    public class NextIterationEventArgs : EventArgs
+    {
+        private INetworkNode node;
+        public NextIterationEventArgs(INetworkNode currentNode)
+        {
+            this.node = currentNode;
+        }
+        public INetworkNode CurrentNode
+        {
+            get
+            {
+                return node;
+            }
+        }
+    }
+}
