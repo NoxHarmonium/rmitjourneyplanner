f04146f7808a48e8847ad71e6eb7f9e82c18e76f
Sean Dawson <sean.dawson@noxharmonium.com>
2011-11-29 02:41:02 +0000

    -Added a global initilization for the proxy

---
+++ UnitTests/GlobalConfig.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+namespace UnitTests
+{
+    [TestClass]
+    public static class InitTestEnv
+    {
+        [AssemblyInitialize]
+        public static void Initialize(TestContext context)
+        {
+            RmitJourneyPlanner.CoreLibraries.DataAccess.ConnectionInfo.Proxy =
+                new System.Net.WebProxy("http://aproxy.rmit.edu.au:8080", false, null, new NetworkCredential("s3229159", "MuchosRowlies1"));
+        }
+    }
+}
