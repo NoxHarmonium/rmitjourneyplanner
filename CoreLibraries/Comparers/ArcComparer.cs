3e44d2455ac215145bba70d05cfd574d294d251d
Sean Dawson <sean.dawson@noxharmonium.com>
2011-11-29 22:26:38 +0000

    -Created a new Compareres namespace and moved all the comparers into it.

---
+++ CoreLibraries/Comparers/ArcComparer.cs
@@ -0,0 +1,31 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ArcComparer.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace RmitJourneyPlanner.CoreLibraries.Comparers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Types;
+
+    /// <summary>
+    /// Compare object for sorting lists of arcs
+    /// </summary>
+    public class ArcComparer : IComparer<Arc>
+    {
+        /// <summary>
+        /// Compares 2 arcs and returns the difference between them.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public int Compare(Arc x, Arc y)
+        {
+            return (x.Time - y.Time).Milliseconds;
+        }
+    }
+}
---
+++ CoreLibraries/Comparers/NodeTimeComparer.cs
@@ -0,0 +1,31 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="NodeComparer.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace RmitJourneyPlanner.CoreLibraries.Comparers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using DataProviders;
+
+    /// <summary>
+    /// Compares to nodes on the basis on total time traversed.
+    /// </summary>
+    public class NodeTimeComparer : IComparer<INetworkNode>
+    {
+        /// <summary>
+        /// Compares 2 nodes on the total time. Sorts descending.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public int Compare(INetworkNode x, INetworkNode y)
+        {
+            return (y.TotalTime - x.TotalTime).Milliseconds;
+        }
+    }
+}
