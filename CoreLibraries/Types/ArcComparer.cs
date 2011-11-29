b494a4bb910a10917ae91bab477bf1b5d749ddf4
Sean Dawson <sean.dawson@noxharmonium.com>
2011-11-29 06:01:25 +0000

    -Made a comparer for arc types that simple works on time. This is used to sort arcs by time for greedy algorithms.

---
+++ CoreLibraries/Types/ArcComparer.cs
@@ -0,0 +1,30 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ArcComparer.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace RmitJourneyPlanner.CoreLibraries.Types
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// Compare object for sorting lists of arcs
+    /// </summary>
+    public class ArcComparer : IComparer<Arc>
+    {
+        /// <summary>
+        /// Compares 2 arcs and returns the difference between them.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public int Compare(Arc x, Arc y)
+        {
+            return (x.Time - y.Time).Milliseconds;
+        }
+    }
+}
