bc3b51ed95388bc577589dbf9eb25abf4d4375e3
Sean Dawson <sean.dawson@noxharmonium.com>
2011-11-30 08:02:27 +0000

    -Added a class to hold utilities for caching.

---
+++ CoreLibraries/Caching/CacheTools.cs
@@ -0,0 +1,49 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Tools.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace RmitJourneyPlanner.CoreLibraries.Caching
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.IO;
+    using System.Xml;
+    /// <summary>
+    /// Caching tools.
+    /// </summary>
+    public class CacheTools
+    {
+        /// <summary>
+        /// De escapes an XML string.
+        /// </summary>
+        /// <param name="xml"></param>
+        /// <returns></returns>
+        public static string Deescape(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return s;
+
+
+            string returnString = s;
+
+            returnString = returnString.Replace("&apos;", "'");
+
+            returnString = returnString.Replace("&quot;", "\"");
+
+            returnString = returnString.Replace("&gt;", ">");
+
+            returnString = returnString.Replace("&lt;", "<");
+
+            //returnString = returnString.Replace("&amp;", "&");
+
+
+
+            return returnString;
+        }
+
+    }
+}
