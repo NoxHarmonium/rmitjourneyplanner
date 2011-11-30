84a94f1dd69180643ccbc2854634e50e5d5845b0
Sean Dawson <sean.dawson@noxharmonium.com>
2011-11-30 08:08:22 +0000

    -Added a lot more caching. -Implemented new interface.

--- CoreLibraries/DataProviders/TramNetworkProvider.cs
+++ CoreLibraries/DataProviders/TramNetworkProvider.cs
@@ -25,7 +25,9 @@ namespace RmitJourneyPlanner.CoreLibraries.DataProviders
         LocationCache lCache = new LocationCache("YarraTrams");
         NodeCache<TramStop> nCache = new NodeCache<TramStop>("YarraTrams");
         RouteCache rCache = new RouteCache("YarraTrams");
+        NodeRouteCache nrCache = new NodeRouteCache("YarraTrams");
         TramTrackerAPI api = new TramTrackerAPI();
+        ScheduleCache sCache = new ScheduleCache("YarraTrams");
 
         /// <summary>
         /// Gets a DataSet containing all the data pertaining to a node.
@@ -62,15 +64,16 @@ namespace RmitJourneyPlanner.CoreLibraries.DataProviders
         public List<INetworkNode> GetNodesAtLocation(Positioning.Location location, double radius)
         {
             List<INetworkNode> nodes = new List<INetworkNode>();
-            List<string> ids = lCache.GetIdsInRadius(location, radius);
-            foreach (string id in ids)
+            List<string[]> ids = lCache.GetIdsInRadius(location, radius);
+            foreach (string[] id in ids)
             {
-                TramStop stop = nCache.GetNode(id, this);
+                TramStop stop = nCache.GetNode(id[0], this);
+                
                 if (stop == null)
                 {                   
-                    stop = new TramStop(id,this);                   
+                    stop = new TramStop(id[0],this);
                 }
-                
+                stop.CurrentRoute = id[1];
                 nodes.Add(stop);             
                 
             }
@@ -118,17 +121,40 @@ namespace RmitJourneyPlanner.CoreLibraries.DataProviders
             Route route = new Route(routeId, new TramStop(upIds[0], this), new TramStop(upIds[upIds.Count - 1],this));
             foreach(string id in upIds)
             {
-                route.AddNode(new TramStop(id, this),true);
+                TramStop stop = new TramStop(id, this);
+                stop.CurrentRoute = routeId;
+                route.AddNode(stop,true);
             }
             foreach (string id in downIds)
             {
-                route.AddNode(new TramStop(id, this), false);
+                TramStop stop = new TramStop(id, this);
+                stop.CurrentRoute = routeId;
+                route.AddNode(stop, false);
             }
-
+            node.CurrentRoute = routeId;
             
             return route.GetAdjacent(node);
 
         }
+
+        /// <summary>
+        /// Gets the network node that is at the specfied location
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns></returns>
+        public INetworkNode GetNodeAtLocation(Location location)
+        {
+            string id = lCache.GetIdFromLocation(location);
+            if (id != null)
+            {
+                return new TramStop(lCache.GetIdFromLocation(location), this);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
 
 
         /// <summary>
@@ -142,26 +168,58 @@ namespace RmitJourneyPlanner.CoreLibraries.DataProviders
         {
             if (source is TramStop && destination is TramStop)
             {
-                //First we generate the routes that intersect both nodes. 
-                DataSet sRouteData = api.GetMainRoutesForStop(source.ID);
-                DataSet dRouteData = api.GetMainRoutesForStop(destination.ID);
+                List<string> sRoutes = nrCache.GetRoutes(source);
+                List<string> dRoutes = nrCache.GetRoutes(destination);
+                
 
-                if (dRouteData.Tables.Count < 1 || sRouteData.Tables.Count < 1)
+                //First we generate the routes that intersect both nodes. 
+                if (sRoutes == null)
+                {
+                    sRoutes = new List<string>();
+                    DataSet sRouteData = api.GetMainRoutesForStop(source.ID);
+                    if (sRouteData.Tables.Count != 0)
+                    {
+                        foreach (DataRow sRow in sRouteData.Tables[0].Rows)
+                        {
+                            sRoutes.Add(sRow[0].ToString());
+                            nrCache.AddCacheEntry(source, sRow[0].ToString());
+                        }
+                    }
+                }
+
+                if (dRoutes == null)
+                {
+                    dRoutes = new List<string>();
+                    DataSet dRouteData = api.GetMainRoutesForStop(destination.ID);
+                    if (dRouteData.Tables.Count != 0)
+                    {
+                        foreach (DataRow dRow in dRouteData.Tables[0].Rows)
+                        {
+                            dRoutes.Add(dRow[0].ToString());
+                            nrCache.AddCacheEntry(destination, dRow[0].ToString());
+                        }
+                    }
+                }
+
+
+
+
+                if (dRoutes.Count < 1 || sRoutes.Count < 1)
                 {
                     return new List<Arc>();
                 }
 
                 List<string> xRows = new List<string>();
 
-                foreach (DataRow sRow in sRouteData.Tables[0].Rows)
+                foreach (string sRow in sRoutes)
                 {
-                    foreach (DataRow dRow in dRouteData.Tables[0].Rows)
+                    foreach (string dRow in dRoutes)
                     {
-                        if (sRow[0].ToString() == dRow[0].ToString())
+                        if (sRow == dRow)
                         {
-                            if (!xRows.Contains(sRow[0].ToString()))
+                            if (!xRows.Contains(sRow))
                             {
-                                xRows.Add(sRow[0].ToString());
+                                xRows.Add(sRow);
                             }
                         }
                     }
@@ -173,27 +231,73 @@ namespace RmitJourneyPlanner.CoreLibraries.DataProviders
                 List<Arc> arcs = new List<Arc>();
                 foreach (string route in xRows)
                 {
-                    DataTable data = api.GetSchedulesCollection(source.ID, route, false, requestTime).Tables[0];
+
+                    DataSet scheduleData = sCache.GetScheduleCollection(source, route, false, requestTime);
+                    if (scheduleData == null)
+                    {
+                        scheduleData = api.GetSchedulesCollection(source.ID, route, false, requestTime);
+                        sCache.AddScheduleCollection(source, route, false, requestTime,scheduleData);
+                    }
+                    DataTable data = scheduleData.Tables[0];
                     if (data.Rows.Count > 0)
                     {
-                        DateTime bestDepartureTime = (DateTime)data.Rows[0]["PredictedArrivalDateTime"];
+                        DateTime bestDepartureTime = default(DateTime);
+                        foreach (DataRow row in data.Rows)
+                        {
+                            if ((DateTime)data.Rows[0]["PredictedArrivalDateTime"] >= requestTime)
+                            {
+                                bestDepartureTime = (DateTime)data.Rows[0]["PredictedArrivalDateTime"];
+                                break;
+                            }
+                        }
+                        if (bestDepartureTime == default(DateTime))
+                        {
+                            throw new Exception("No suitable departure times?");
+                        }
                         DateTime bestArrivalTime = default(DateTime);
                         string tripID = data.Rows[0]["TripID"].ToString();
-                        DataTable tripData = api.GetSchedulesForTrip(tripID, bestDepartureTime).Tables[0];
-                        foreach (DataRow row in tripData.Rows)
+
+                        DataSet tripData = sCache.GetTripSchedule(tripID, bestDepartureTime);
+                        if (tripData == null)
                         {
-                            if (row["StopNo"].ToString() == destination.ID)
+                            tripData = api.GetSchedulesForTrip(tripID, bestDepartureTime);
+                            sCache.AddTripSchedule(tripID, bestDepartureTime, tripData);
+                        }
+                        
+                        DataTable tripTable = tripData.Tables[0];
+                        
+                        
+                        bool correctDir = false;
+                        foreach (DataRow row in tripTable.Rows)
+                        {
+                            if (row["StopNo"].ToString() == source.ID)
+                            {
+                                correctDir = true;
+                            }
+                            if (row["StopNo"].ToString() == destination.ID && correctDir)
                             {
                                 bestArrivalTime = (DateTime)row["ScheduledArrivalDateTime"];
                                 break;
                             }
 
                         }
+                       
 
                         if (bestArrivalTime == default(DateTime))
                         {
                             break;
                         }
+
+                        if (bestArrivalTime < bestDepartureTime)
+                        {
+                            throw new Exception("Arriving before leaving??");
+                        
+                        }
+                        if (bestArrivalTime == bestDepartureTime)
+                        {
+                            //bestArrivalTime += new TimeSpan(0, 0, 1);
+                        }
+
 
                         TimeSpan travelTime = bestArrivalTime - bestDepartureTime;
                         TimeSpan waitingTime = bestDepartureTime - requestTime;
@@ -202,7 +306,7 @@ namespace RmitJourneyPlanner.CoreLibraries.DataProviders
                         NetworkArc arc = new NetworkArc(source, destination, totalTime,
                                          GeometryHelper.GetStraightLineDistance(source.Latitude, source.Longitude, destination.Latitude, destination.Longitude),
                                          bestDepartureTime,
-                                         "YarraTrams");
+                                         "YarraTrams",route);
                         arcs.Add(arc);
 
                     }
