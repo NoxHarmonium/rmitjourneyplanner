37c561b7475684d215008366255d14db10553294
Sean Dawson <sean.dawson@noxharmonium.com>
2011-12-02 00:54:53 +0000

    -Fixed output to show time of each leg instead of total. -Added time to name so it appears on each pin in google maps.

--- WebInterface/GetNext.aspx.cs
+++ WebInterface/GetNext.aspx.cs
@@ -55,7 +55,7 @@ namespace WebInterface
                         TimeSpan totalTime = current.TotalTime;
                         if (current.Parent != null)
                         {
-                            totalTime = current.Parent.TotalTime;
+                            totalTime = current.TotalTime - current.Parent.TotalTime;
                         }
                         
                         element.Append(String.Format(
@@ -66,7 +66,7 @@ namespace WebInterface
                                     "\t\t\t\"Type\": \"{4}\"\n",
                                     current.Latitude,
                                     current.Longitude,
-                                    current.ID,
+                                    current.ID + String.Format(" ({0})",current.TotalTime),
                                     totalTime,
                                     travelType));
 
