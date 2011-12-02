9cc0f843a5d557c2098b28dcde4c3aa0945444c8
Sean Dawson <sean.dawson@noxharmonium.com>
2011-12-02 06:38:03 +0000

    -Too many features and bug fixes to list.

--- WebInterface/GetNext.aspx.cs
+++ WebInterface/GetNext.aspx.cs
@@ -13,95 +13,127 @@ namespace WebInterface
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            RouteSolver.NextStep();
-            INetworkNode current = RouteSolver.Current;
-            StringBuilder JSON = new StringBuilder("{\n \"paths\": [{\n");
-           
-            INetworkNode[] nodelist = new INetworkNode[] { RouteSolver.Current, RouteSolver.Best };
-            int count = 0;
-
-            foreach (INetworkNode node in nodelist)
+            bool success = false;
+            if (Request.Params["next"] == "true")
             {
-                current = node;
+                RouteSolver.NextStep();
+            }
+            if (!success)
+            {
+                INetworkNode current = RouteSolver.Current;
+                StringBuilder JSON = new StringBuilder("{\n \"paths\": [{\n");
 
+                INetworkNode[] nodelist = new INetworkNode[] { RouteSolver.Current, RouteSolver.Best };
+                int count = 0;
 
-                JSON.Append("\t\"Route\":[ \n ");
-
-                StringBuilder elements = new StringBuilder();
-                
-                
-
-                if (current != null)
+                foreach (INetworkNode node in nodelist)
                 {
-                    bool ifirst = true;
-                    while (current != null)
+                    current = node;
+
+
+                    JSON.Append("\t\"Route\":[ \n ");
+
+                    StringBuilder elements = new StringBuilder();
+
+                    string baseImage;
+
+                    if (count == 0)
                     {
-                        string travelType;
-                        if (current.Parent == null)
+                        baseImage = "images/icons/red/";
+                    }
+                    else
+                    {
+                        baseImage = "images/icons/blue/";
+                    }
+
+                    if (current != null)
+                    {
+                        bool ifirst = true;
+                        while (current != null)
                         {
-                            travelType = "Arrive at the start";
-                        }
-                        else
-                            if ((current.GetType() == current.Parent.GetType()) && current.GetType() == typeof(TramStop))
+                            string travelType;
+                            string image = baseImage;
+                            if (current.Parent == null)
                             {
-                                travelType = "Catch a tram to ";
+                                travelType = "Begin at the ";
+                                image += "../number_0.png";
+                            }
+                            else
+                                
+                                if ((current.GetType() == current.Parent.GetType()) && current.GetType() == typeof(TramStop))
+                                {
+                                    travelType = "Catch a tram to ";
+                                    image += "tramway.png";
+                                }
+                                else
+                                {
+                                    travelType = "Walk to ";
+                                    image += "pedestriancrossing.png";
+                                }
+                            StringBuilder element = new StringBuilder();
+                            element.Append("\t\t{\n");
+                            TimeSpan totalTime = current.TotalTime;
+                            if (current.Parent != null)
+                            {
+                                totalTime = current.TotalTime - current.Parent.TotalTime;
+                            }
+
+
+
+
+
+                            element.Append(String.Format(
+                                        "\t\t\t\"Latitude\": \"{0}\",\n" +
+                                        "\t\t\t\"Longitude\": \"{1}\",\n" +
+                                        "\t\t\t\"Name\": \"{2}\",\n" +
+                                        "\t\t\t\"TotalTime\": \"{3}\",\n" +
+                                        "\t\t\t\"Image\": \"{4}\",\n" +
+                                        "\t\t\t\"Type\": \"{5}\"\n",
+                                        current.Latitude,
+                                        current.Longitude,
+                                        current.ID + String.Format(" ({0})", current.TotalTime),
+                                        totalTime,
+                                        image,
+                                        travelType));
+
+                            current = current.Parent;
+                            if (!ifirst)
+                            {
+                                element.Append("\t\t},\n");
                             }
                             else
                             {
-                                travelType = "Walk to ";
+                                element.Append("\t\t}\n");
+                                ifirst = false;
                             }
-                        StringBuilder element = new StringBuilder();
-                        element.Append("\t\t{\n");
-                        TimeSpan totalTime = current.TotalTime;
-                        if (current.Parent != null)
-                        {
-                            totalTime = current.TotalTime - current.Parent.TotalTime;
-                        }
-                        
-                        element.Append(String.Format(
-                                    "\t\t\t\"Latitude\": \"{0}\",\n" +
-                                    "\t\t\t\"Longitude\": \"{1}\",\n" +
-                                    "\t\t\t\"Name\": \"{2}\",\n" +
-                                    "\t\t\t\"TotalTime\": \"{3}\",\n" +
-                                    "\t\t\t\"Type\": \"{4}\"\n",
-                                    current.Latitude,
-                                    current.Longitude,
-                                    current.ID + String.Format(" ({0})",current.TotalTime),
-                                    totalTime,
-                                    travelType));
 
-                        current = current.Parent;
-                        if (!ifirst)
-                        {
-                            element.Append("\t\t},\n");
-                        }
-                        else
-                        {
-                            element.Append("\t\t}\n");
-                            ifirst = false;
-                        }
-                        
 
-                        elements.Insert(0, element);
-                        
+                            elements.Insert(0, element);
+
+                        }
                     }
-                }
-                JSON.Append(elements);
-                if (count == 1)
-                {
-                    JSON.Append("\t]}\n");
-                }
-                else
-                {
-                    count++;
-                    JSON.Append("\t]},{\n");
-                }
+                    JSON.Append(elements);
+                    if (count == 1)
+                    {
+                        JSON.Append("\t]}\n");
+                    }
+                    else
+                    {
+                        count++;
+                        JSON.Append("\t]},{\n");
+                    }
 
-                
+
+                }
+                JSON.Append("], \"iteration\" : \"" + RouteSolver.CurrentIteration.ToString() +"\"}\n");
+                Response.Clear();
+                Response.Write(JSON);
             }
-            JSON.Append("]}\n");
-            Response.Clear();
-            Response.Write(JSON);
+            else
+            {
+                Response.Clear();
+                Response.Write("{ \"success\": \"true\" }");
+            }
         }
     }
 }
