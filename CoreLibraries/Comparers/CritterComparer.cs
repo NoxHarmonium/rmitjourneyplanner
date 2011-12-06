3d7ebd06e6bcd4f4fe38d2f6d10c0574290cd12e
Sean Dawson <sean.dawson@noxharmonium.com>
2011-12-06 22:56:08 +0000

    Added comparer for critters

---
+++ CoreLibraries/Comparers/CritterComparer.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CritterComparer.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace RmitJourneyPlanner.CoreLibraries.Comparers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class CritterComparer : IComparer<Types.Critter>
+    {
+
+        public int Compare(Types.Critter x, Types.Critter y)
+        {
+            return (int)Math.Round(y.Fitness - x.Fitness);
+        }
+    }
+}
