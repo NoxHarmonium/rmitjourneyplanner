949cde84fc99a343a4b15655761e9755b547876a
Sean Dawson <sean.dawson@noxharmonium.com>
2011-12-06 23:03:14 +0000

    Added comments.

--- CoreLibraries/Comparers/CritterComparer.cs
+++ CoreLibraries/Comparers/CritterComparer.cs
@@ -12,11 +12,21 @@ namespace RmitJourneyPlanner.CoreLibraries.Comparers
     using System.Text;
 
     /// <summary>
-    /// TODO: Update summary.
+    /// Compares 2 critters based on thier fitness.
     /// </summary>
     public class CritterComparer : IComparer<Types.Critter>
     {
-
+        /// <summary>
+        /// Compares 2 critters based on thier fitness.
+        /// </summary>
+        /// <param name="x">
+        /// The first critter.
+        /// </param>
+        /// <param name="y">
+        /// The second critter.
+        /// </param>
+        /// <returns>
+        /// </returns>
         public int Compare(Types.Critter x, Types.Critter y)
         {
             return (int)Math.Round(y.Fitness - x.Fitness);
