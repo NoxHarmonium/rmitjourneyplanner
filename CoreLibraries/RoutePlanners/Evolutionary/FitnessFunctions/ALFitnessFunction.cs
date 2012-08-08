14364ec0f3eb381e04a2f4c0e782f89121b9ed20
Sean Dawson <sean.dawson@noxharmonium.com>
2012-08-08 05:40:30 +0000

    Removed obsolete methods.

--- CoreLibraries/RoutePlanners/Evolutionary/FitnessFunctions/ALFitnessFunction.cs
+++ CoreLibraries/RoutePlanners/Evolutionary/FitnessFunctions/ALFitnessFunction.cs
@@ -9,7 +9,6 @@ namespace RmitJourneyPlanner.CoreLibraries.RoutePlanners.Evolutionary.FitnessFun
     using System;
     using System.Collections;
     using System.Collections.Generic;
-    using System.Data;
     using System.Globalization;
     using System.IO;
     using System.Linq;
@@ -440,109 +439,6 @@ namespace RmitJourneyPlanner.CoreLibraries.RoutePlanners.Evolutionary.FitnessFun
 
 
         #region Methods
-
-        /// <summary>
-        ///   The calculate time.
-        /// </summary>
-        /// <param name="routeid"> The routeid. </param>
-        /// <param name="id1"> The id 1. </param>
-        /// <param name="id2"> The id 2. </param>
-        /// <param name="departureTime"> The departure time. </param>
-        /// <returns> </returns>
-        [Obsolete]
-        private TimeSpan CalculateTime(int routeid, int id1, int id2, DateTime departureTime)
-        {
-            string dowFilter = string.Empty;
-            for (int j = 0; j < (int)departureTime.DayOfWeek; j++)
-            {
-                dowFilter = dowFilter.Insert(0, "_");
-            }
-
-            dowFilter += "0%";
-
-            string query =
-                string.Format(
-                    @"select st1.ServiceID, 
-                    min(st1.DepartTime), st1.DepartTime,st2.ArriveTime frOM tblSST st1
-                    INNER JOIN tblSST st2
-                    ON st1.ServiceID=st2.ServiceID
-                    WHERE st1.RouteID={3} AND st1.DepartTime>{2} AND st1.SourceID={0} AND st1.DOW LIKE '{4}' 
-                    AND st2.DestID={1}
-                        ",
-                    id1,
-                    id2,
-                    departureTime.ToString("Hmm"),
-                    routeid,
-                    dowFilter);
-
-            DataTable result = this.properties.Database.GetDataSet(query);
-
-            // departureTime.
-            if (result.Rows.Count > 0 && result.Rows[0][0].ToString() != string.Empty)
-            {
-                DateTime arrivalTime = this.ParseDate(result.Rows[0]["ArriveTime"].ToString());
-                arrivalTime += departureTime.Date - default(DateTime).Date;
-
-                TimeSpan output = arrivalTime - departureTime;
-                if (output.Ticks < 0)
-                {
-                    // throw new Exception("Negitive time span detected.");
-                    // Logger.Log(this,"WARNING: Negitive timespan between nodes detected!");
-                    return default(TimeSpan);
-                }
-
-                return output;
-            }
-            // Logger.Log(this,"WARNING: Null timespan between nodes detected!");
-            return default(TimeSpan);
-        }
-
-        /// <summary>
-        ///   The parse date.
-        /// </summary>
-        /// <param name="date"> The date. </param>
-        /// <returns> </returns>
-        [Obsolete]
-        private DateTime ParseDate(string date)
-        {
-            if (date == "9999")
-            {
-                date = "0000";
-            }
-
-            if (date == "0" || date == "-1")
-            {
-                date = "0000";
-            }
-
-            int minutes = Convert.ToInt32(date.Substring(date.Length - 2, 2));
-            int hours = Convert.ToInt32(date.Substring(0, date.Length - 2));
-            var dt = new DateTime();
-            int days = 0;
-            if (hours >= 24)
-            {
-                days = 1;
-                hours -= 24;
-            }
-
-            dt = dt.Add(new TimeSpan(days, hours, minutes, 0));
-
-            /*
-            if (!DateTime.TryParseExact(date, "Hmm", null, DateTimeStyles.None, out dt))
-            {
-                if (date.Substring(0, 2) == "24")
-                {
-                    date = "00" + date.Substring(2, 2);
-                    if (!DateTime.TryParseExact(date, "Hmm", null, DateTimeStyles.None, out dt))
-                    {
-                        throw new Exception("Time parsing error");
-                    }
-                }
-            }
-            */
-
-            return dt;
-        }
 
         #endregion
     }
