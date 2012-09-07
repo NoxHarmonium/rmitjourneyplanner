afeb3ba5ce496fa965b20e43f1bde0142dcec348
Sean Dawson <sean.dawson@noxharmonium.com>
2012-09-07 06:45:09 +0000

    New project is now building properly.

---
+++ JRPCServer/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace JRPCServer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// Main entry point of the program.
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
