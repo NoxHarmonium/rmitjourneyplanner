cc185e04037560f08d2abb436d92ac66204e17ca
Sean Dawson <sean.dawson@noxharmonium.com>
2012-11-06 22:27:31 +0000

    Added namespace documentation.

---
+++ CoreLibraries/DataAccess/NamespaceDoc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RmitJourneyPlanner.CoreLibraries.DataAccess
+{
+    /// <summary>
+    /// The <see cref="RmitJourneyPlanner.CoreLibraries.DataAccess"/> namespace contains low level classes that access various data sources such as databases and APIs. 
+    /// These classes are usually used by a higher level data provider such as the classes from <see cref="RmitJourneyPlanner.CoreLibraries.DataProviders"/> to create a more 
+    /// developer friendly layer.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    class NamespaceDoc
+    {
+    }
+}
