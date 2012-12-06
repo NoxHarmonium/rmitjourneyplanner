7e317b54db0c9a9c22b5d77f80b48b5df1f86fe5
Sean Dawson <sean.dawson@noxharmonium.com>
2012-12-06 04:01:35 +0000

    Logging is now 100% Stylecop compliant.

--- CoreLibraries/Logging/Logger.cs
+++ CoreLibraries/Logging/Logger.cs
@@ -13,17 +13,21 @@ namespace RmitJourneyPlanner.CoreLibraries.Logging
     /// The handler for log events.
     /// </summary>
     /// <param name="sender">
+    /// The sender of the event.
     /// </param>
     /// <param name="message">
+    /// The event message.
     /// </param>
     public delegate void LogEventHandler(object sender, string message);
 
     /// <summary>
-    /// The handler for progress update events.
+    /// The handler for progress update events. 
     /// </summary>
     /// <param name="sender">
+    /// The sender of the event.
     /// </param>
     /// <param name="progress">
+    /// The progress update value (usually out of 100).
     /// </param>
     public delegate void ProgressEventHandler(object sender, int progress);
 
@@ -49,11 +53,13 @@ namespace RmitJourneyPlanner.CoreLibraries.Logging
         #region Public Methods and Operators
 
         /// <summary>
-        /// Trigger a log event causing the message to be recieved by all registered objects.
+        /// Trigger a log event causing the message to be received by all registered objects.
         /// </summary>
         /// <param name="sender">
+        /// The sender of the event.
         /// </param>
         /// <param name="message">
+        /// The event message.
         /// </param>
         public static void Log(object sender, string message)
         {
@@ -84,11 +90,13 @@ namespace RmitJourneyPlanner.CoreLibraries.Logging
         }
 
         /// <summary>
-        /// Trigger a progress update event causing a message to be recieved by all registered objects.
+        /// Trigger a progress update event causing a message to be received by all registered objects.
         /// </summary>
         /// <param name="sender">
+        /// The sender of the event.
         /// </param>
         /// <param name="progress">
+        /// The progress values (usually out of 100).
         /// </param>
         public static void UpdateProgress(object sender, int progress)
         {
