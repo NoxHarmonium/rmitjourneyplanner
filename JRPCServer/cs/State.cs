026a1e75e3746d02ff35bc9b1a6895d3b0687672
Sean Dawson <sean.dawson@noxharmonium.com>
2012-12-13 14:02:05 +1100

    Added state object for general server data persitance.

---
+++ JRPCServer/cs/State.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="State.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace JRPCServer.cs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// A class the represents the state of a RPC Server. 
+    /// </summary>
+    public class State
+    {
+        public State()
+        {
+            LogEventHooked = false;
+        }
+
+        public bool LogEventHooked { get; set; }
+    }
+}
